9068a533a4d0b5cac259492deed5c12c04073575
arvfin <s160822003@student.ubaya.ac.id>
2024-10-03 14:47:41 +0700

    what we play

--- app/src/main/java/com/ubaya/bernadya/AchievementActivity.kt
+++ app/src/main/java/com/ubaya/bernadya/AchievementActivity.kt
@@ -6,26 +6,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ubaya.bernadya.databinding.ActivityAchievementBinding
 import com.ubaya.bernadya.databinding.ActivityWhatWePlayBinding
 
-class WhatWePlayActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityWhatWePlayBinding
+class AchievementActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAchievementBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityWhatWePlayBinding.inflate(layoutInflater)
+        binding = ActivityAchievementBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnhome.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-        }
+        //binding.btnhome.setOnClickListener{
+        //    val intent = Intent(this,MainActivity::class.java)
+        //    startActivity(intent)
+        //}
 
-        binding.btnAchievement.setOnClickListener{
-            val intent = Intent(this,AchievementActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
